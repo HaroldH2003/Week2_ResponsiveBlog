670052a320652c5b182474af16892f9f4bc17d5b
Manh Huy Hoang <mhuy2003@gmail.com>
2018-08-13 21:45:29 +0700

    submission
    submission

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+
+  $(window).scroll(function () {
+    //if you hard code, then use console
+    //.log to determine when you want the 
+    //nav bar to stick.  
+    console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 40) {
+      $('nav').addClass("navbar-fixed scrolling-navbar");
+      $('.scrolling-navbar a').css('color', '#fff');
+    }
+    if ($(window).scrollTop() < 41) {
+      $('nav').removeClass("navbar-fixed scrolling-navbar");
+      $('nav a').css({"color": "rgb(0,0,0)"});
+    }
+    if ($(window).scrollTop() > 750) {
+      $('.scrolling-navbar').css('background-color', '#fff');
+      $('.scrolling-navbar a').css('color', '#000');
+    }
+    if ($(window).scrollTop() < 749) {
+      $('.scrolling-navbar').css('background-color', 'transparent');
+      $('.scrolling-navbar a').css('color', '#fff');
+    }
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $('#back-to-top').fadeIn();
+      $('#back-to-top').tooltip('show');
+    } else {
+      $('#back-to-top').fadeOut();
+      $('#back-to-top').tooltip('hide');
+    }
+  });
+  // scroll body to 0px on click
+  $('#back-to-top').click(function () {
+    $('#back-to-top').tooltip('hide');
+    $('body,html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+});
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+// On click
+hamburger.addEventListener("click", function () {
+  // Toggle class "is-active"
+  hamburger.classList.slideToggle("is-active");
+  // Do something else, like open/close menu
+});
